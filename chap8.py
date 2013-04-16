78ec737df1c5471153d49962da09eb8af954b741
Elizabeth Tenorio <betsky98@gmail.com>
2013-04-15 21:43:45 -0400

    Fix chap8.py

--- chap8.py
+++ chap8.py
@@ -13,16 +13,16 @@ A program to encrypt a message in ROT13 by shifting a letter tthrough the alphab
 #key = 5 
 #message = 'Python'
 
+#key is the number of places the letters of the message should be moved during encryption
 
 def rotate_word(message, key):
     new_message = ''
-    s = message.upper()
-    end = ord('Z') + 1
+    s = message.lower()
     for letter in s:
         new_ord = key + ord(letter) 
-        if new_ord > end:
-            #uppercase letters can have ord from 65 to 90
-            new_ord = end - new_ord + 65
+        if new_ord > 122:
+            #lowerrcase letters can have ord from 65 to 90
+            new_ord = new_ord -26
             new_letter = chr(new_ord)
         else:
             new_letter = chr(new_ord)
@@ -30,5 +30,6 @@ def rotate_word(message, key):
         new_message = new_message + new_letter
     print new_message.lower()
 
-rotate_word('python', 1 )
-rotate_word('python', 10 )
+rotate_word('xyz', 1 )
+rotate_word('xyz', 10 )
+rotate_word('PYTHON', 13)
