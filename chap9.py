678b6c106bb5c3cbe65dd277302fac707136070e
Elizabeth Tenorio <betsky98@gmail.com>
2013-04-15 23:25:38 -0400

    Add 9.3 and 9.4 to chap9.py

--- chap9.py
+++ chap9.py
@@ -47,12 +47,38 @@ per_e =  e_word_count * 100 / (no_e_word_count + e_word_count)
 print "Percentage of words in the list without 'e':", per_e 
 
 #Ex. 9.3:
-print "Ex. 9.3: Returns True if the word does net use any forbidden letters"
+print "Ex. 9.3: Returns True if the word does not use any forbidden letters"
+def avoids(word, forbidden):
+    for letter in forbidden:
+        if letter in word:
+            return False
+        else:
+            return True
 
+print avoids('beth', 'e')
+print avoids('beth', 'a')
+print avoids('beth', 'qr')
+print avoids('beth', 'bqr')
 
-
+forbidden = raw_input("Enter a string of forbidden letters: ")
+print "The following are words that avoid the forbidden letters: "
+print forbidden
+fin = open('words.txt')
+for line in fin:
+    word = line.strip()
+    if avoids(word, forbidden):
+        print word
 
 #Ex. 9.4:
-
+print "This function takes a word and a string of letters, and that returns True if the word contains only letters in the list"
+def uses_only(word, list):
+    for letter in word:
+        if letter in list:
+            return True
+        else: 
+            return False
+print uses_only('bottom', 'btm')
+print uses_only('bottom', 'btmx')
+print uses_only('bottom', 'x')
 
 
